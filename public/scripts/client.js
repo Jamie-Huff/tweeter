f34f895a29c4c3138c0028af474fc6e18e64384e
jamiehuff737@gmail.com <jamiehuff737@gmail.com>
2021-07-14 20:12:20 +0000

    adding function to renderTweets for every tweet we have stored in our database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,40 +7,62 @@
  */
 $(document).ready(function() {
 const createTweetElement = function(tweetData) {
-  const $html = `
+  const $html = 
+  `
   <article id="old-tweet-article">
-  <header id="old-tweet-header">  
-    <div id="old-tweet-name">
-      <img id="old-tweet-profile-photo" src="${tweetData.user.avatars}">
-      <h4 id="old-tweet-name-text">${tweetData.user.name}</h4>
-    </div>
-    <h4 id="old-tweet-handle">${tweetData.user.handle}</h4>
-  </header>
-  <div id="old-tweet-text">${tweetData.content.text}</div>
-  <footer id="old-tweet-footer">
-    <div id="post-created-date">${timeago.format(tweetData.created_at)}</div>
-    <div id="post-links">
-      <i id='post-report' class="fas fa-flag"></i>
-      <i id='post-retweet' class="fas fa-retweet"></i>
-      <i id='post-like' class="fas fa-heart"></i>
-    </div>
-  </footer>
-</article>`
+    <header id="old-tweet-header">  
+      <div id="old-tweet-name">
+        <img id="old-tweet-profile-photo" src="${tweetData.user.avatars}">
+        <h4 id="old-tweet-name-text">${tweetData.user.name}</h4>
+      </div>
+      <h4 id="old-tweet-handle">${tweetData.user.handle}</h4>
+    </header>
+    <div id="old-tweet-text">${tweetData.content.text}</div>
+    <footer id="old-tweet-footer">
+      <div id="post-created-date">${timeago.format(tweetData.created_at)}</div>
+      <div id="post-links">
+        <i id='post-report' class="fas fa-flag"></i>
+        <i id='post-retweet' class="fas fa-retweet"></i>
+        <i id='post-like' class="fas fa-heart"></i>
+      </div>
+    </footer>
+  </article>
+  `
 
-  return $('#old-tweet-section').append($html)
+  return $html
   }
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
 //$('#old-tweet-section').append(createTweetElement(tweetData));
+
+  const renderTweets = function(tweetsArray) {
+    for (let i = 0; i < tweetsArray.length; i++) {
+      $('#old-tweet-section').append(createTweetElement(tweetsArray[i]))
+    }
+  }
+  renderTweets(data)
 })
